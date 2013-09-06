dca70e5cb5b311ea974266bacc06db9ab14a7b7f
Ahmed Azaan <azaan@outlook.com>
2013-09-06 20:59:08 +0530

    implement filter

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -3,12 +3,18 @@ function NewsCtrl($scope, $http) {
 
     $scope.news = [];
     $scope.categories = [];
+    $scope.catFilter = {};
 
     $http.get('endpoint/news.json').success(function(data) {
         $scope.news = data;
         for (var i = 0; i < data.length; i++) {
             $scope.categories.push(data[i].category);
+            $scope.catFilter[data[i].category] = true;
         }
         console.log(data);
     });
+
+    $scope.categoryFilter = function(item) {
+        return $scope.catFilter[item.category];
+    };
 }
